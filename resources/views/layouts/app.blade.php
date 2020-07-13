50a320731c777d05ad46b4ed6e1bd4dc4f33df0a
henrypandales <henrypandales@hotmail.com>
2020-07-13 14:27:04 -0500

    Titulo en Layouts SisLaravel by @henrypandales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                <h1> {{ config('app.name', 'Laravel') }}</h1>
+                <h1> {{ config('app.name', 'Laravel') }}</h1> <b>SisLaravel by @henrypandales </b>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
